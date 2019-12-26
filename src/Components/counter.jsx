41d33ec72bf490b229905c3413bb189fb2105f14
louisgurung <louisgurung@yahoo.com>
2019-12-26 20:24:58 +0545

    updating state, rendering classes dynamically

---
+++ src/Components/counter.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: []
+  };
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  //   styles = {
+  //     fontSize: 50,
+  //     fontWeight: "bold"
+  //   };
+
+  //in state can write ->imgUrl: "https://picsum.photos/500"
+  render() {
+    return (
+      <div>
+        {/* <span style={{ fontSize: 20 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span> */}
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
+
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+
+        {this.state.tags.length === 0 &&
+          "Can print a statement with 'and' if the condition is true"}
+        {this.renderTags()}
+      </div>
+    );
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "ZERO" : count;
+  }
+}
+
+export default Counter;
