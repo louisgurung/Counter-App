8c85be16235c1149588ec2edf7066641e89091ec
louisgurung <louisgurung@yahoo.com>
2019-12-29 20:30:16 +0545

    implemented increment, delete

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: []
-  };
-
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
@@ -26,43 +21,49 @@ class Counter extends Component {
         <span className={this.getBadgeClass()}>{this.formatCount()}</span>
 
         <button
-          onClick={() => this.handleIncrement(1)}
-          className="btn btn-secondary btn-sm"
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm m-2"
         >
           Increment
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
 
-        {this.state.tags.length === 0 &&
-          "Can print a statement with 'and' if the condition is true"}
-        {this.renderTags()}
+        {/* {this.state.tags.length === 0 &&
+          "Can print a statement with 'and' if the condition is true"} */}
+        {/* {this.renderTags()} */}
       </div>
     );
   }
 
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-  };
+  // handleIncrement = product => {
+  //   console.log(product);
+  //   this.setState({ count: this.state.count + 1 });
+  // };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  // renderTags() {
+  //   // if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  //   return (
+  //     // <ul>
+  //     //   {this.state.tags.map(tag => (
+  //     //     <li key={tag}>{tag}</li>
+  //     //   ))}
+  //     // </ul>
+  //   );
+  // }
   getBadgeClass() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { count } = this.props.counter;
     return count === 0 ? "ZERO" : count;
   }
 }
